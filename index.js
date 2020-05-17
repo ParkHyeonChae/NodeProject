62c7249a0a3fe49c717725fbf2a540adf700e50b
ParkHyeonChae <park19996@naver.com>
2020-05-18 01:21:57 +0900

    mongoose설치, mongoDB연걸, 설정값 secrets.json 분리

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // 서버 실행 npm run start (package.json script 설정 명렁어)
 
-const express = require('express') // express 모듈 가졍괴
+var secrets = require('./secrets.json');
+const express = require('express') // express 모듈 가져오기
 const app = express() // 앱 생성
 const port = 5000 // 5000번 포트 백서버
+
+const mongoose = require('mongoose') // mongoose 설치, mongodb 연결
+mongoose.connect('mongodb+srv://' + secrets.mongodb_user + ':' + secrets.mongodb_pw + '@boilerplate-gsiul.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 에러방지 설정값
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!~')) // 루트 디렉토리 hello world 출력
 
